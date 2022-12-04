7003c67669057d6bdb40a3284e34a4ae0c2013b4
zmiin-pm <ziminpm@gmail.com>
2022-12-04 19:32:33 +0200

    change data to example

--- hw_lesson_10/hw_12_custom_map.py
+++ hw_lesson_10/hw_12_custom_map.py
@@ -5,7 +5,7 @@ def custom_map(function: Callable, *iterables: Iterable) -> Iterable:
     return [function(iterable) for iterable in zip(*iterables)]
 
 print(
-    custom_map(sum, [1, 2, 3], [4, 5, 6])
+    custom_map(sum, [1, 2, 3], [3, 5, 0, 5])
 )
 print(
     custom_map(max, [1, 5, 3], [2, 4, 6])
