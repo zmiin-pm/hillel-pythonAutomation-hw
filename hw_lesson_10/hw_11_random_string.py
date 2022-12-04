273b56c01aa9a36d9746d97d824d6f60aef1d2f0
zmiin-pm <ziminpm@gmail.com>
2022-12-04 14:01:53 +0200

    add hw_11_random_string

---
+++ hw_lesson_10/hw_11_random_string.py
@@ -0,0 +1,14 @@
+from random import choice
+
+
+def get_random_string(length: int) -> str:
+    numbers = [chr(i) for i in range(48, 58)]
+    big_letters = [chr(i) for i in range(65, 91)]
+    small_letters = [chr(i) for i in range(97, 123)]
+    all_signs = ''.join(numbers + big_letters + small_letters)
+    return ''.join(choice(all_signs) for _ in range(length))
+
+
+print(
+    get_random_string(30)
+)
