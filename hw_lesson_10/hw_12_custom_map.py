908d41ef3d5aa8d65b17b7ba677dd001a2453081
zmiin-pm <ziminpm@gmail.com>
2022-12-04 19:39:06 +0200

    add __doc__

--- hw_lesson_10/hw_12_custom_map.py
+++ hw_lesson_10/hw_12_custom_map.py
@@ -2,7 +2,13 @@ from typing import Iterable, Callable
 
 
 def custom_map(function: Callable, *iterables: Iterable) -> Iterable:
+    """
+    2. Написать функцию, которая возвращает список результатов выполнения заданной функции, к соответствующим элементам переданных итерируемых объектов.
+    Если переданные итерируемые объекты разной длины, то результат сформировать по кратчайшему итерируемому объекту.
+    custom_map(sum, [1, 2, 3], [3, 5, 0, 5]) -> [4, 7, 3]
+    """
     return [function(iterable) for iterable in zip(*iterables)]
+
 
 print(
     custom_map(sum, [1, 2, 3], [3, 5, 0, 5])
