4742e2a57aa4b3a49a97a107b74d264038ad4957
zmiin-pm <ziminpm@gmail.com>
2022-12-04 14:39:38 +0200

    add get_random_string_2

--- hw_lesson_10/hw_11_random_string.py
+++ hw_lesson_10/hw_11_random_string.py
@@ -10,5 +10,16 @@ def get_random_string(length: int) -> str:
 
 
 print(
-    get_random_string(30)
+    get_random_string(20)
+)
+
+def get_random_string_2(length: int) -> str:
+    numbers = {chr(i) for i in range(48, 58)}
+    big_letters = {chr(i) for i in range(65, 91)}
+    small_letters = {chr(i) for i in range(97, 123)}
+    numbers.update(big_letters, small_letters)
+    return ''.join(numbers.pop() for _ in range(length))
+
+print(
+    get_random_string_2(20)
 )
