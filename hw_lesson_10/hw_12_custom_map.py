367afe575cad2c61b8375ac1efa8aaf6b49e6f03
zmiin-pm <ziminpm@gmail.com>
2022-12-05 12:30:45 +0200

    add checking the number of iterables

--- hw_lesson_10/hw_12_custom_map.py
+++ hw_lesson_10/hw_12_custom_map.py
@@ -1,18 +1,22 @@
-from typing import Iterable, Callable
+from typing import Iterable, Callable, List
 
 
-def custom_map(function: Callable, *iterables: Iterable) -> Iterable:
+def custom_map(function: Callable, *iterables: Iterable) -> List:
     """
     2. Написать функцию, которая возвращает список результатов выполнения заданной функции, к соответствующим элементам переданных итерируемых объектов.
     Если переданные итерируемые объекты разной длины, то результат сформировать по кратчайшему итерируемому объекту.
     custom_map(sum, [1, 2, 3], [3, 5, 0, 5]) -> [4, 7, 3]
     """
-    return [function(iterable) for iterable in zip(*iterables)]
+    iterables = iterables[0] if len(iterables) == 1 else zip(*iterables)
+    return [function(iterable) for iterable in iterables]
 
 
 print(
-    custom_map(sum, [1, 2, 3], [3, 5, 0, 5])
+    custom_map(sum, [1, 2, 3], [6, 7, 3])
 )
 print(
     custom_map(max, [1, 5, 3], [2, 4, 6])
+)
+print(
+    custom_map(str, [17, 23])
 )
