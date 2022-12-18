75bef95da7821d07b37c78d2629d58d83f93af85
zmiin-pm <ziminpm@gmail.com>
2022-12-18 19:12:01 +0200

    add endless generator

--- hw_lesson_11/hw_14_lecture_schedule.py
+++ hw_lesson_11/hw_14_lecture_schedule.py
@@ -3,7 +3,7 @@
 Первое занятие произошло 17.10.2022. Всего 32 занятия.
 Вывести список всех занятий в таком формате (номера лекций выровнены по правому краю)
 """
-from datetime import timedelta, date, time
+from datetime import timedelta, date, time, datetime
 
 
 def gen_lesson_date():
@@ -28,4 +28,31 @@ gen = gen_lesson_date()
 for counter, value in enumerate(gen):
     print(
         f'Lecture {counter + 1:>2}: {value.strftime("%d %b %Y")} {lesson_time.strftime("%H:%M")}'
+    )
+
+
+print('\n')
+# second variant
+
+
+def gen_lesson_date_time(first_date_time, first_call=True):
+    delta_from_mon_to_thurs = timedelta(days=3)
+    delta_from_thurs_to_mon = timedelta(days=4)
+    while True:
+        if first_call:
+            first_call = False
+            lesson_date_time = first_date_time
+            yield lesson_date_time
+        lesson_date_time += delta_from_mon_to_thurs
+        yield lesson_date_time
+        lesson_date_time += delta_from_thurs_to_mon
+        yield lesson_date_time
+
+
+first_lesson = datetime(2022, 10, 17, 19, 15)
+gen_1 = gen_lesson_date_time(first_lesson)
+
+for lesson_number in range(32):
+    print(
+        f'Lecture {lesson_number + 1:>2}: {next(gen_1).strftime("%d %b %Y %H:%M")}'
     )
